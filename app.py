cabea18d86d0696343fa876c961a4ad8b3375024
Dariochua <2402261@sit.singaporetech.edu.sg>
2025-07-16 11:45:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ if pod_file:
 
 # ----------------------------- Idle Section -----------------------------
 st.header("🕒 Idle Time, Mileage & Max Speed Analysis")
-st.markdown("ℹ️ **Go to Detrack > Today's Jobs > Vehicles > Click on download route for each individual riders and upload all here.**")
+st.markdown("ℹ️ **Go to Detrack > Today's Jobs > Vehicles > Click on download route for each individual riders.**")
 rider_files = st.file_uploader("Upload multiple rider Excel files (vehicle route)", type=["xlsx", "xls"], accept_multiple_files=True, key="idle")
 
 if rider_files:
